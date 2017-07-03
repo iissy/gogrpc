cb96222791fb4d29c51b87543383a3d25c6325cc
hemin <pinbor@iissy.com>
2017-07-03 13:40:40 +0800

    go grpc demo

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "log"
+    "net"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    pb "helloworld"
+    "google.golang.org/grpc/reflection"
+)
+ 
+const (
+    port = ":50052"
+)
+ 
+type server struct {}
+ 
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+    return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+ 
+func main() {
+    lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
